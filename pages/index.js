dde1b0357fb66a7f47badf653e0652a54582571e
Martín M <skydiver@users.noreply.github.com>
2020-01-15 17:08:40 -0300

    demo page using tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Index = () => <div>Hello World</div>;
+const Index = () => (
+  <div className="h-screen flex justify-center items-center bg-gray-200">
+    <h1 className="text-5xl font-bold text-blue-700">Next.js + Tailwind CSS</h1>
+  </div>
+);
 
 export default Index;
